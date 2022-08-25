ca802bd40c19ab37838da441c380875dac72657c
Rohit Singh <rohit1995sgh@gmail.com>
2022-08-26 00:22:55 +0530

    Movies, Casts and Dialouges models and tables

---
+++ database/migrations/2022_08_25_184956_create_casts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCastsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('casts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('movie_id')->constrained('movies')->onDelete('cascade');
+            $table->string('character_name');
+            $table->string('name');
+            $table->string('gender');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('casts');
+    }
+}
---
+++ database/migrations/2022_08_25_185058_create_dialouges_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDialougesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dialouges', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cast_id')->constrained('casts')->onDelete('cascade');
+            $table->text('dialouge');
+            $table->time('start', $precision = 3)->nullable();
+            $table->time('end', $precision = 3)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dialouges');
+    }
+}
